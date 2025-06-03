5bb39aead0d5b127a9407666c8dfe87be85cdb8e
peethree <pieterschoptkont@gmail.com>
2025-06-03 18:20:05 +0200

    change in usage, fixed test

--- test.sh
+++ test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # open first terminal, cd into communication dir, execute user1's client
-kitty @ launch --type=window --title dealer1 bash -c 'eval "$(zoxide init bash)"; z communication && ./dealer user1 user2; exec bash'
+kitty @ launch --type=window --title dealer1 bash -c 'eval "$(zoxide init bash)"; z communication && ./dealer user2; exec bash'
 
 # pause
 sleep 1
 
 # open second terminal, cd into communication dir, execute user2's client
-kitty @ launch --type=window --title dealer2 bash -c 'eval "$(zoxide init bash)"; z communication && ./dealer user2 user1; exec bash'
+kitty @ launch --type=window --title dealer2 bash -c 'eval "$(zoxide init bash)"; z communication && ./dealer user1; exec bash'
