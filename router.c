66b598b7c701dc726db651e1e5a68a2c4d8750f6
peethree <pieterschoptkont@gmail.com>
2025-06-03 12:54:42 +0200

    turned the router into a curve server. There is still a bug (segmentation fault) when the program gets interrupted by ctrl+C

--- router.c
+++ router.c
@@ -15,37 +15,26 @@
 // dealers use the router's public key to authenticate it
 // router uses the dealer's public kye to authorize or deny access
 
-
-zcertstore_t *load_certs(char* directory) 
-{
-    zcertstore_t *certstore = zcertstore_new(directory);
-    if (!certstore) {
-        fprintf(stderr, "Failed to create certificate store\n");
-        return NULL;
-    }    
-    return certstore;
-}
-
 // kill router if perpetually blocked: ps aux | grep router ----- kill -9 with associated ./router pid
 int main()
 {
     // load certs from certificate directory
-    // char* directory = "keys";
-    // zcertstore_t *certstore = load_certs(directory); 
-    // if (!certstore){
-    //     printf("No certificates loaded\n");
-    //     return -1;
-    // }
+    char* directory = "keys";
+    zcertstore_t *certstore = zcertstore_new(directory);
+    if (!certstore){
+        fprintf(stderr, "Failed to create certificate store\n");
+        return -1;
+    }    
 
-    // zcertstore_print(certstore);
+    zcertstore_print(certstore);
 
     // look up router public key and apply its certificate to the socket
-    // char* router_pub_key = "A9Iz>yq^pr*w=I1.vTE)NDguZ0[#>GXl-hZ=B>&0";
-    // zcert_t *router_cert = zcertstore_lookup(certstore, router_pub_key);
-    // if (!router_cert) {
-    //     printf("Certificate does not match the store lookup\n");
-    //     return -1;
-    // }
+    const char* router_pub_key = "A9Iz>yq^pr*w=I1.vTE)NDguZ0[#>GXl-hZ=B>&0";
+    zcert_t *router_cert = zcertstore_lookup(certstore, router_pub_key);
+    if (!router_cert) {
+        printf("Certificate does not match the store lookup\n");
+        return -1;
+    }
 
     zsock_t *router = zsock_new(ZMQ_ROUTER);
     if (!router){
@@ -53,29 +42,26 @@ int main()
         return 2;
     }
     
-    // zcert_apply(router_cert, router);
-    // printf("applied cert to router\n");
+    zcert_apply(router_cert, router);
+    printf("applied cert to router\n");
 
-    // // set to act as CURVE server
-    // zsock_set_curve_server(router, true);
-    // printf("set curve server\n");
+    // set to act as CURVE server
+    zsock_set_curve_server(router, true);
+    printf("set curve server\n");
     
     // zauth actor instance
-    // zactor_t *auth = zactor_new(zauth, router_cert);
-    // if (!auth){
-    //     printf("Unable to create authentication actor\n");
-    //     zsock_destroy(&router);
-    //     zcert_destroy(&router_cert);
-    //     return 3;
-    // }
-
-    // // configure authentication to use CURVE
-    // zstr_sendx(auth, "CURVE", directory, NULL);
-    // zsock_wait(auth);
+    zactor_t *auth = zactor_new(zauth, router_cert);
+    if (!auth){
+        printf("Unable to create authentication actor\n");
+        zsock_destroy(&router);
+        zcert_destroy(&router_cert);
+        zcertstore_destroy(&certstore);
+        return 3;
+    }
+    printf("authentication actor with certificate policy started\n");
 
     int rc = zsock_bind(router, "tcp://*:5555");
     assert (rc != -1);
-
     printf("router started successfully...\n");        
 
     // non-blocking:
@@ -103,24 +89,24 @@ int main()
         }     
 
         // add sender's public key's message frame
-        // zframe_t *sender_pub_key = zmsg_pop(msg);
-        // zcertstore_lookup()
+        zframe_t *sender_pub_key = zmsg_pop(msg);
+        zframe_print(sender_pub_key, "sender pub key:");
 
         // pop sender id
         zframe_t *sender_id = zmsg_pop(msg); 
-        // printf("sender id: %s\n", zframe_strhex(sender_id));
+        printf("sender id: %s\n", zframe_strhex(sender_id));
 
         // pop recipient id
         zframe_t *rec_id = zmsg_pop(msg);
-        // printf("recipient id: %s\n", zframe_strhex(rec_id));
+        printf("recipient id: %s\n", zframe_strhex(rec_id));
 
         // pop msg content
         zframe_t *message_data = zmsg_pop(msg);   
-        // zframe_print(message_data, "cipher: ");
+        zframe_print(message_data, "cipher: ");
         
         // reply ... forward to recipient
         zmsg_t *reply = zmsg_new();
-        //zmsg_append(reply, &sender_pub_key);
+        zmsg_append(reply, &sender_pub_key);
         zmsg_append(reply, &rec_id);        // ROUTING: destination frame
         zmsg_append(reply, &sender_id);     // CONTENT: original sender ID (as body)
         zmsg_append(reply, &message_data);  // CONTENT: message
@@ -130,10 +116,10 @@ int main()
         zmsg_destroy(&msg);
     }
     // zpoller_destroy(&poller);
-    // zactor_destroy(&auth);
-    zsock_destroy(&router);
-    // zcert_destroy(&router_cert);
-    // zcertstore_destroy(&certstore);
+    zactor_destroy(&auth);   
+    zsock_destroy(&router); 
+    zcert_destroy(&router_cert);
+    zcertstore_destroy(&certstore);    
 
     return 0;
 }
